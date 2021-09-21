06e54d7e91c787033ec9f22e94eb4c8e34615c88
Manuel Muiña <manuel_muina@gitlab.com>
2021-09-21 19:31:31 +0200

    delete python_2_compatible decorator

--- django_celery_monitor/models.py
+++ django_celery_monitor/models.py
@@ -9,7 +9,6 @@ from django.conf import settings
 
 from celery import states
 from celery.events.state import heartbeat_expires
-from celery.five import python_2_unicode_compatible
 
 from . import managers
 
@@ -17,7 +16,6 @@ ALL_STATES = sorted(states.ALL_STATES)
 TASK_STATE_CHOICES = sorted(zip(ALL_STATES, ALL_STATES))
 
 
-@python_2_unicode_compatible
 class WorkerState(models.Model):
     """The data model to store the worker state in."""
 
@@ -59,7 +57,6 @@ class WorkerState(models.Model):
         return mktime(self.last_heartbeat.timetuple())
 
 
-@python_2_unicode_compatible
 class TaskState(models.Model):
     """The data model to store the task state in."""
 
